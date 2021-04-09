e94f6904495633de540b6c1a68f2e6300dd5bce4
marcgrant21 <marcgrant10@gmail.com>
2021-04-09 18:06:28 -0500

    update views

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request
+from flask import render_template, request, jsonify
+from .forms import UploadForm
+from werkzeug.utils import secure_filename
+import os
 
 ###
 # Routing for your application.
@@ -31,22 +34,17 @@ def index(path):
 
 @app.route("/api/upload", methods=["POST"])
 def uploads():
-    
     form = UploadForm()
-    
-    
     if form.validate_on_submit():
         description = form.description.data
         photoData = form.photo.data
         filename = secure_filename(photoData.filename)
-        
         try:
             photoData.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return jsonify(message="200", filename=filename, description=description)
         except Exception as e:
-            print e
+            print(e)
             return jsonify(errors=["internal Error"])
-
     return jsonify(errors=form_errors(form))
 
 
